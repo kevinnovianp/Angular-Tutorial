aca339d6d9bef142dde5ad2fe5d5502994464804
kevinnovianp <kevinnovianprafajar@gmail.com>
2022-08-31 16:12:59 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'Angular 12'
+
+  // printAlert(name:any){
+  //   alert(name)
+  // }
+  // printAlert2(name:string | number){
+  //   alert(name)
+  // }
+
+  // printConsole(val:string){
+  //   console.warn(val)
+  // }
+
+  // displayVal:string=''
+  // getValue(val:string){
+  //   console.warn(val)
+  //   this.displayVal = val
+  // }
+
+  // count=0
+  // act(type:string){
+  //   type==='plus' ? this.count++ : this.count--
+  // }
+
+  // name = 'Peter'
+  // disable = false
+
+  // show='yes'
+
+  // color='orange'
+
+  // users = ['Alpha', 'Beta', 'Charlie', 'Gamma', 'Epsilon']
+  // usersDetail = [
+  //   {name: 'Alpha', email:'alpha@gmail.com', phone:'1111', account:['facebook', 'email', 'linkedin']},
+  //   {name: 'Beta', email:'beta@gmail.com', phone:'2222', account:['facebook', 'email', 'instagram']},
+  //   {name: 'Gamma', email:'gamma@gmail.com', phone:'3333', account:['facebook', 'email', 'youtube']},
+  //   {name: 'Delta', email:'delta@gmail.com', phone:'4444', account:['facebook', 'email', 'yahoo']},
+  //   {name: 'Epsilon', email:'epsilon@gmail.com', phone:'5555', account:['facebook', 'email', 'twitter']},
+  // ]
+
+  // bgColor = 'green'
+  // updateColor(){
+  //   this.color = 'skyblue'
+  //   this.bgColor = 'black'
+  // }
+
+  userData:any={}
+  getFormData(data:NgForm){
+    console.warn(data)
+    this.userData=data
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UserAuthModule } from './user-auth/user-auth.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+// import { CountryComponent } from './country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    // CountryComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuthModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
